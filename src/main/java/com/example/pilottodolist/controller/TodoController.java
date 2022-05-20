662b1a6c041492a4df05160c0712c7e1ad73f208
= <=>
2022-05-21 00:20:02 +0900

    FEAT. 동시성 테스트

--- src/main/java/com/example/pilottodolist/controller/TodoController.java
+++ src/main/java/com/example/pilottodolist/controller/TodoController.java
@@ -6,6 +6,7 @@ import com.example.pilottodolist.dto.TodoRequestDto;
 import com.example.pilottodolist.dto.TodoResponseDto;
 import com.example.pilottodolist.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,6 +21,7 @@ public class TodoController {
     /**
      * 할일 추가
      */
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/todo")
     public TodoResponseDto.CREATED createTodo(@RequestBody @Valid TodoRequestDto.CREATED created) {
         return todoService.create(created);
@@ -28,6 +30,7 @@ public class TodoController {
     /**
      * 상태 수정
      */
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/{todoId}/progress")
     public void changeProgress(@PathVariable("todoId") Long id) {
         todoService.changeProgress(id);
@@ -36,6 +39,7 @@ public class TodoController {
     /**
      * 체크(COMPLETED)한 할일 삭제
      */
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping(value = "/delete-todos")
     public void deleteTodoList(@RequestBody @Valid TodoRequestDto.DELETED deleted) {
         todoService.deleteTodoList(deleted);
@@ -44,6 +48,7 @@ public class TodoController {
     /**
      * 전체 조회
      */
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/todoList")
     public TodoListInfo<TodoResponseDto.GET> getTodoList(@Valid TodoRequestDto.GET request) {
         List<TodoResponseDto.GET> todoList = todoService.getTodoList(Progress.of(request.getProgress()));
@@ -53,6 +58,7 @@ public class TodoController {
     /**
      * 전체상태 수정
      */
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/all-progress")
     public TodoResponseDto.ALLUPDATE updateAllProgress(@RequestBody @Valid TodoRequestDto.ALLUPDATE updateDto) {
         return todoService.updateAllProgress(updateDto.getCurrentState());
--- src/test/java/com/example/pilottodolist/service/TodoServiceTest.java
+++ src/test/java/com/example/pilottodolist/service/TodoServiceTest.java
@@ -2,70 +2,54 @@ package com.example.pilottodolist.service;
 
 import com.example.pilottodolist.domain.Progress;
 import com.example.pilottodolist.domain.Todo;
+import com.example.pilottodolist.dto.TodoRequestDto;
 import com.example.pilottodolist.repository.TodoRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TodoServiceTest {
 
-    @Mock
-    TodoRepository todoRepository;
-    @InjectMocks
-    TodoService todoService;
-
-    private void sleep(int mills) {
-        try {
-            Thread.sleep(mills);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updateWithSleep(Long id) {
-        todoService.changeProgress(id);
-        sleep(1000);
-    }
+    @Autowired
+    private TodoRepository todoRepository;
+    @SpyBean
+    private TodoService todoService;
 
     @Test
     @DisplayName(value = "동시에 progress 상태 변경 테스트")
     void concurrentChangeProgressTest() throws InterruptedException {
         //given
         Todo todo = todoRepository.save(new Todo("Hello World"));
-        Runnable userA = () -> {
-            updateWithSleep(todo.getId());
-        };
-
-        Runnable userB = () -> {
-            updateWithSleep(todo.getId());
-        };
-
-        Thread threadA = new Thread(userA);
-        threadA.setName("***********Thread A************");
-        Thread threadB = new Thread(userB);
-        threadB.setName("***********Thread B************");
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
 
         //when
-        threadA.start();
-        threadB.start();
+        for (int i = 0; i<2; i++) {
+            executor.execute(() -> todoService.changeProgress(todo.getId()));
+        }
 
-    }
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.MINUTES);
 
-    @Test
-    @DisplayName(value = "progress 상태 수정 중일 때 삭제 테스트")
-    void concurrentChangeAndDeleteProgressTest() {
-
+        //then
+        Todo afterTodo = todoRepository.findById(todo.getId()).get();
+        assertThat(afterTodo.getVersion()).isEqualTo(1);
+        assertThat(afterTodo.getProgress()).isEqualTo(Progress.COMPLETED);
     }
 }
